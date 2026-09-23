a45b2fa6263206fb5017d0083728c6b689f22502
AnnaAb830 <abramencko.ana@yandex.ru>
2026-09-23 20:05:11 +0500

    Add project files.

---
+++ Tyuiu.AbramenkoAS.Sprint1.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AbramenkoAS.Sprint1.Task0.V20.Lib;
+
+namespace Tyuiu.AbramenkoAS.Sprint1.Task0.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(13, res);
+        }
+    }
+}
